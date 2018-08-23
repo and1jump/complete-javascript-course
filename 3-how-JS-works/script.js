0a281cb52cc23bc22b4a9a073222a937ba019577
Daniil <onejump9@gmail.com>
2018-08-23 13:37:53 +0300

    finished section 3, learned how JS really works, a little practice with the keyword (this)

--- 3-how-JS-works/script.js
+++ 3-how-JS-works/script.js
@@ -1,77 +1,103 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//function
+// calculateAge(1990);
 
+// function calculateAge(year) {
+//   console.log(2016 - year);
+// }
 
+// retirement(1956);
 
+// let retirement = function(year) {
+//   console.log(65 - (2016 - year));
+// };
 
+//variables
 
+// console.log(age);
+// let age = 23;
 
-
-
-
-
-
-
-
-
-
+// function foo() {
+//   let age = 65;
+//   console.log(age);
+// }
+// foo();
+// console.log(age);
 
 ///////////////////////////////////////
 // Lecture: Scoping
 
-
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
+// var a = "Hello!";
+// first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// function first() {
+//   var b = "Hi!";
+//   second();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
-
-
+//   function second() {
+//     var c = "Hey!";
+//     console.log(a + b + c);
+//   }
+// }
 
 // Example to show the differece between execution stack and scope chain
 
-/*
-var a = 'Hello!';
-first();
+// var a = "Hello!";
+// first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// function first() {
+//   var b = "Hi!";
+//   second();
 
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
+//   function second() {
+//     var c = "Hey!";
+//     third();
+//   }
+// }
 
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
-
-
+// function third() {
+//   var d = "John";
+//   console.log(a + b + c + d);
+// //   console.log(a + d);
+// }
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
 
+// calculateAge(1985);
 
+// function calculateAge(year) {
+//   console.log(2016 - year);
+//   console.log(this);
+// }
 
+let john = {
+  name: "John",
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(2016 - this.yearOfBirth);
+    /*
+    function innerFunc() {
+      console.log(this);
+    }
+    innerFunc();
+    */
+  }
+};
 
+john.calculateAge();
 
+let mike = {
+  name: "Mike",
+  yearOfBirth: 1984
+};
 
-
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
