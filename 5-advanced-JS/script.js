c42825674340876b0f33e9c58e31c365b87e4ec0
Daniil <onejump9@gmail.com>
2018-09-17 15:34:07 +0300

    First Class Functions: Passing function as argument, Functions returning functions. What is IIFE.

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -53,40 +53,131 @@ let jane = Object.create(personProto, {
 
 // Primitives vs objects
 
-// Primitives
-let a = 23;
-let b = a;
-a = 46;
-console.log(a);
-console.log(b);
+// // Primitives
+// let a = 23;
+// let b = a;
+// a = 46;
+// console.log(a);
+// console.log(b);
 
-// Objects
-let obj1 = {
-  name: "John",
-  age: 26
-};
+// // Objects
+// let obj1 = {
+//   name: "John",
+//   age: 26
+// };
 
-let obj2 = obj1;
-obj1.age = 30;
+// let obj2 = obj1;
+// obj1.age = 30;
 
-console.log(obj1.age);
-console.log(obj2.age);
+// console.log(obj1.age);
+// console.log(obj2.age);
 
-// Functions
+// // Functions
 
-let age = 23;
-let obj = {
-  name: "Dan",
-  city: "Sochi"
-};
+// let age = 23;
+// let obj = {
+//   name: "Dan",
+//   city: "Sochi"
+// };
 
-function change(a, b) {
-  a = 30;
-  b.city = "Amsterdam";
+// function change(a, b) {
+//   a = 30;
+//   b.city = "Amsterdam";
+// }
+
+// change(age, obj);
+
+// console.log(age);
+// console.log(obj.city);
+
+////////////////////////////////////////////////////
+// Lecture: Passing function as argument
+
+/*
+var yaers = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
 }
 
-change(age, obj);
+function calculateAge(el) {
+  return 2016 - el;
+}
 
-console.log(age);
-console.log(obj.city);
+function isFullAge(el) {
+  return el >= 18;
+}
 
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - 0.67 * el);
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(yaers, calculateAge);
+console.log(ages);
+
+var fullAge = arrayCalc(ages, isFullAge);
+console.log(fullAge);
+
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(rates);
+*/
+
+////////////////////////////////////////////////////
+// Lecture: Functions returning functions
+
+/*
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function(name) {
+      console.log(name + ", can you explaine what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function(name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
+  } else {
+    return function(name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
+}
+
+const designerQuestion = interviewQuestion("designer");
+const teacherQuestion = interviewQuestion("teacher");
+const nameQuestion = interviewQuestion("Dan");
+
+teacherQuestion("Dan");
+designerQuestion("Dan");
+nameQuestion("Dan");
+*/
+
+////////////////////////////////////////////////////
+// Lecture: IIFE
+
+/*
+function game() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+}
+game();
+*/
+
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+// console.log(score());
+
+(function(goodLuck) {
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(5);
