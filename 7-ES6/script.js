54da2ff3ded97c8652b6c9a5e7dfb3be3bdad392
Daniil <onejump9@gmail.com>
2018-12-07 12:24:36 +0300

    ES6: Arrays, the spread operator, rest and default parameters, Maps, Classes, Classes with Subclasses

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -276,6 +276,8 @@ console.log(retirement);
 ////////////////////////////////////////
 // Arrays
 
+/*
+
 const boxes = document.querySelectorAll(".box");
 
 //ES 5
@@ -331,3 +333,285 @@ console.log(ages[full.indexOf(true)]);
 // ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+
+let arr = [1, 2, "dan"];
+
+const [age1, age2, name] = arr;
+
+console.log(age1, age2, name);
+
+*/
+
+///////////////////////////////////////////
+// Spread operator
+
+/*
+
+function addFourAges(a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+var ages = [18, 30, 12, 21];
+
+//ES5
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+//ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ["John", "Jane", "Mark"];
+const familyMiller = ["Mary", "Bob", "Ann"];
+
+const bigFamily = [...familySmith, ...familyMiller, "Dan"];
+console.log(bigFamily);
+
+const h = document.querySelector("h1");
+const boxes = document.querySelectorAll(".box");
+const all = [h, ...boxes];
+
+console.log(all);
+
+Array.from(all).forEach(cur => (cur.style.color = "purple"));
+
+*/
+
+///////////////////////////////////////////
+// Rest parameters
+
+/*
+
+//ES5
+
+function isFullAge5() {
+  //console.log(arguments);
+
+  var argsArr = Array.prototype.slice.call(arguments);
+
+  argsArr.forEach(function(cur) {
+    console.log(2018 - cur >= 18);
+  });
+}
+
+//isFullAge5(1990, 2005, 1965);
+//isFullAge5(1990, 2005, 1965, 2016, 1976);
+
+//ES6
+
+function isFullAge6(...years) {
+  years.forEach(cur => console.log(2016 - cur >= 18));
+}
+
+isFullAge6(1990, 2005, 1965);
+
+*/
+
+//ES5
+/*
+function isFullAge5(limit) {
+  var argsArr = Array.prototype.slice.call(arguments, 1);
+
+  argsArr.forEach(function(cur) {
+    console.log(2018 - cur >= 18);
+  });
+}
+
+isFullAge5(21, 1990, 2005, 1965);
+//isFullAge5(1990, 2005, 1965, 2016, 1976);
+
+//ES6
+
+function isFullAge6(limit, ...years) {
+  years.forEach(cur => console.log(2016 - cur >= limit));
+}
+
+isFullAge6(1990, 2005, 1965);
+
+///////////////////////////////////////////
+// Default parameters
+
+// ES5
+
+/*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+  lastName === undefined ? (lastName = "Smith") : (lastName = lastName);
+  nationality === undefined
+    ? (nationality = "russian")
+    : (nationality = nationality);
+
+  
+}
+
+
+// ES6
+
+function SmithPerson(
+  firstName,
+  yearOfBirth,
+  lastName = "Smith",
+  nationality = "american"
+) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
+}
+
+var dan = new SmithPerson("Dan", 1995);
+var emily = new SmithPerson("Emily", 1990, "Diaz", "spanish");
+
+///////////////////////////////////////////
+// Maps
+
+const question = new Map();
+
+question.set(
+  "question",
+  "What is the official name of the lateste major JavaScript version?"
+);
+question.set(1, "ES5");
+question.set(2, "ES6");
+question.set(3, "ES2015");
+question.set(4, "ES7");
+question.set("correct", 3);
+question.set(true, "Correct answet :D");
+question.set(false, "Wrong, please try again!");
+
+console.log(question.get("question"));
+// console.log(question.size);
+
+// if (question.has(4)) {
+//   question.delete(4);
+// }
+// question.clear(); delete all
+
+// question.forEach((value, key) =>
+// console.log(`This is ${key}, and it's set to ${value}`)
+// );
+
+for (let [key, value] of question.entries()) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const ans = parseInt(prompt("Wright the correct answer"));
+console.log(question.get(ans === question.get("correct")));
+*/
+
+///////////////////////////////////////////
+// Classes
+
+// ES5
+
+/*
+
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calcAge = function() {
+  var age = new Date().getFullYear - this.yearOfBirth;
+  console.log(age);
+};
+
+var john5 = new Person5("John", 1990, "teacher");
+
+//ES6
+
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calcAge() {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log("Hey there!");
+  }
+}
+
+
+
+Person6.greeting();
+
+*/
+
+///////////////////////////////////////////
+// Classes and Subclasses
+
+// ES5
+
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calcAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+};
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job);
+  this.olympicGames = olympicGames;
+  this.medals = medals;
+};
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals);
+};
+
+var johnAthlete5 = new Athlete5("John", 1990, "swimmer", 3, 10);
+
+johnAthlete5.calcAge();
+johnAthlete5.wonMedal();
+
+//ES6
+
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calcAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+}
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
+
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals);
+  }
+}
+
+const johnAthlete6 = new Athlete6("John", 1990, "swimmer", 3, 10);
+
+johnAthlete6.wonMedal();
+johnAthlete6.calcAge();
+
