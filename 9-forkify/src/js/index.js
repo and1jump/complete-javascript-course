185b34fb2ddc5faeec87cd45ad984fcd7fa37a25
Daniil <onejump9@gmail.com>
2018-12-21 16:16:44 +0300

    Build the Search View. Remove the now ignored directory node_modules.

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
 /** Global state of the app
  * - Search object
@@ -11,23 +13,25 @@ const state = {};
 
 const controlSearch = async () => {
   // 1) Get query from view
-  const query = "pizza"; //TODO
+  const query = searchView.getInput();
 
   if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
 
     // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) render results on UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", event => {
+elements.searchForm.addEventListener("submit", event => {
   event.preventDefault();
   controlSearch();
 });
