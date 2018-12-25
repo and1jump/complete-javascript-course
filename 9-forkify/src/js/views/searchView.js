57c7d335fdb0bed2e122efd04163a234f5885b85
Daniil <onejump9@gmail.com>
2018-12-25 11:59:49 +0300

    Build the Search View: added Loading Spinner and shortened recipe title

--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -10,6 +10,31 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = "";
 };
 
+/*
+// "Pasta with tomato and spinach"
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato', 'and']
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato', 'and']
+
+*/
+
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    // return result
+    return `${newTitle.join(" ")} ...`;
+  }
+  return title;
+};
+
 const renderRecipe = recipe => {
   const markup = `
                 <li>
@@ -20,7 +45,9 @@ const renderRecipe = recipe => {
   }">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(
+                              recipe.title
+                            )}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
